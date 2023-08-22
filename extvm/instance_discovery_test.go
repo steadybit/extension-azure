790caa766130f080b77d3494aaa278984fce8baf
Ansgar Schulte <1299623+ansgarschulte@users.noreply.github.com>
2023-08-22 17:26:53 +0200

    test: add discovery unit test

--- extvm/instance_discovery_test.go
+++ extvm/instance_discovery_test.go
@@ -33,9 +33,9 @@ func TestGetAllAzureVirtualMachines(t *testing.T) {
 				map[string]any{
 					"name":           "myVm",
 					"type":           "Microsoft.Compute/virtualMachines",
-					"id":             "/subscriptions/42/resourceGroups/rg-1/providers/Microsoft.Compute/virtualMachines/i-0ef9adc9fbd3b19c5",
 					"location":       "westeurope",
 					"subscriptionId": "42",
+          "resourceGroup":  "rg-1",
 					"tags": map[string]any{
 						"tag1": "Value1",
 					},
@@ -43,11 +43,22 @@ func TestGetAllAzureVirtualMachines(t *testing.T) {
 						"hardwareProfile": map[string]any{
 							"vmSize": "Standard_D2s_v3",
 						},
+						"extended": map[string]any{
+							"instanceView": map[string]any{
+                "osVersion": "18.04.5 LTS",
+                "computerName": "dev-demo",
+                "osName": "Ubuntu 18.04.5 LTS",
+								"vmAgent": map[string]any{
+									"vmAgentVersion": "2.7.0",
+								},
+								"powerState": map[string]any{
+									"code": "PowerState/running",
+								},
+							},
+						},
 						"storageProfile": map[string]any{
-							"imageReference": map[string]any{
-								"publisher": "Canonical",
-								"offer":     "UbuntuServer",
-								"sku":       "18.04-LTS",
+							"osDisk": map[string]any{
+								"osType": "Linux",
 							},
 						},
 						"osProfile": map[string]any{
@@ -62,7 +73,7 @@ func TestGetAllAzureVirtualMachines(t *testing.T) {
 							},
 						},
 						"provisioningState": "Succeeded",
-						"vmId":              "0815",
+						"vmId":              "/subscriptions/42/resourceGroups/rg-1/providers/Microsoft.Compute/virtualMachines/i-0ef9adc9fbd3b19c5",
 					},
 				},
 			},
@@ -82,6 +93,17 @@ func TestGetAllAzureVirtualMachines(t *testing.T) {
 	assert.Equal(t, "myVm", target.Label)
 	assert.Equal(t, []string{"myVm"}, target.Attributes["azure-vm.vm.name"])
 	assert.Equal(t, []string{"42"}, target.Attributes["azure-vm.subscription.id"])
+	assert.Equal(t, []string{"/subscriptions/42/resourceGroups/rg-1/providers/Microsoft.Compute/virtualMachines/i-0ef9adc9fbd3b19c5"}, target.Attributes["azure-vm.vm.id"])
+	assert.Equal(t, []string{"Standard_D2s_v3"}, target.Attributes["azure-vm.vm.size"])
+	assert.Equal(t, []string{"Ubuntu 18.04.5 LTS"}, target.Attributes["azure-vm.os.name"])
+	assert.Equal(t, []string{"dev-demo"}, target.Attributes["azure-vm.computer.name"])
+	assert.Equal(t, []string{"18.04.5 LTS"}, target.Attributes["azure-vm.os.version"])
+	assert.Equal(t, []string{"Linux"}, target.Attributes["azure-vm.os.type"])
+	assert.Equal(t, []string{"PowerState/running"}, target.Attributes["azure-vm.power.state"])
+	assert.Equal(t, []string{"/subscriptions/42/resourceGroups/rg-1/providers/Microsoft.Network/networkInterfaces/i-0ef9adc9fbd3b19c5"}, target.Attributes["azure-vm.network.id"])
+	assert.Equal(t, []string{"westeurope"}, target.Attributes["azure-vm.location"])
+	assert.Equal(t, []string{"rg-1"}, target.Attributes["azure-vm.resource-group.name"])
+	assert.Equal(t, []string{"tag1:Value1"}, target.Attributes["azure-vm.tags"])
 	_, present := target.Attributes["label.name"]
 	assert.False(t, present)
 }
