7d6e1b6fbcb543b415e1363139fac72b96ee4cc0
Daniel Reuter <daniel@steadybit.com>
2024-11-21 07:10:10 +0100

    feat: add enrichment for loadtest locations

--- config/config.go
+++ config/config.go
@@ -18,7 +18,7 @@ type Specification struct {
 	AzureUserAssertionString                    string   `json:"azureUserAssertionString" required:"false" split_words:"true"`
 	DiscoveryAttributesExcludesScaleSetInstance []string `json:"discoveryAttributesExcludesScaleSetInstance" required:"false" split_words:"true"`
 	DiscoveryAttributesExcludesVM               []string `json:"discoveryAttributesExcludesVM" required:"false" split_words:"true"`
-	EnrichScaleSetVMDataForTargetTypes          []string `json:"EnrichScaleSetVMDataForTargetTypes" split_words:"true" default:"com.steadybit.extension_jvm.jvm-instance,com.steadybit.extension_container.container,com.steadybit.extension_kubernetes.kubernetes-deployment,com.steadybit.extension_kubernetes.kubernetes-pod,com.steadybit.extension_kubernetes.kubernetes-daemonset,com.steadybit.extension_kubernetes.kubernetes-statefulset,com.steadybit.extension_http.client-location"`
+	EnrichScaleSetVMDataForTargetTypes          []string `json:"EnrichScaleSetVMDataForTargetTypes" split_words:"true" default:"com.steadybit.extension_jvm.jvm-instance,com.steadybit.extension_container.container,com.steadybit.extension_kubernetes.kubernetes-deployment,com.steadybit.extension_kubernetes.kubernetes-pod,com.steadybit.extension_kubernetes.kubernetes-daemonset,com.steadybit.extension_kubernetes.kubernetes-statefulset,com.steadybit.extension_http.client-location,com.steadybit.extension_jmeter.location,com.steadybit.extension_k6.location,,com.steadybit.extension_gatling.location"`
 }
 
 var (
